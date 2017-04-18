1740d7a1aeaf52316a7d4be7178cbee344f44806
Zaynstir <Zaynstir@gmail.com>
2017-04-18 17:06:04 -0500

    Update replace.js

--- replace.js
+++ replace.js
@@ -7,14 +7,15 @@ $(document).ready(function () {
         if (msg.getRay == "receive") {
             ray = msg.sendRay;
             console.log(ray);
-            var elements = document.getElementsByTagName(('*').not("style","script"));
+            //var elements = document.getElementsById('*');
+            var elements = $(document.getElementsByTagName('*')).not("style, script, noscript");
+            //var elements = document.querySelectorAll("*:not('style')");
             for (var k = 0; k < ray.length - 1; k += 2){
                 for (var i = 0; i < elements.length; i++) {
-                    var element = elements[i];
-
+                        	var element = elements[i];
                     for (var j = 0; j < element.childNodes.length; j++) {
                         var node = element.childNodes[j];
-
+                    	console.log((''+elements[j]+'').indexOf("style")+" "+element[j]+" "+(''+elements[j]+'').indexOf("script"));
                         if (node.nodeType === 3) {
                             var text = node.nodeValue;
                             var replace = ray[k];
@@ -31,6 +32,7 @@ $(document).ready(function () {
                     }
                 }console.log("k");
             }
+            console.log(elements);
         }
     });
     
